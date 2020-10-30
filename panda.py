79d66b45ea75c09dc32989ff36f90cbd95d5ec64
CheetSheatOverlode <65246540+CheetSheatOverlode@users.noreply.github.com>
2020-10-29 20:04:15 -0400

    Added token dictionary into panda.py

--- panda.py
+++ panda.py
@@ -1,4 +1,27 @@
+tokens = {
+    "+":"PLUS";
+    "-":"MINUS";
+    "*":"MUL";
+    "/":"DIV";
+    "^":"EXP";
+    "%":"MOD";
+    "#":"COM";
+    "(":"LPAREN";
+    ")":"RPAREN";
+    "[":"LBRACK";
+    "]":"RBRACK";
+    "{":"LBRACE";
+    "}":"RBRACE";
+    "@":"FUNC";
+    "$":"FORM";
+    "&":"AND";
+    "!":"NOT";
+    "|":"OR"
+}
+
+
 def lexer(string:str):
+    global token
     pass
 
 def parser(string:str):
