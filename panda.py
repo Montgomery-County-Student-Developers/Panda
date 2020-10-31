6e5ff2766c51592a5d98299f5a09f62b78e9b3d7
CheetSheatOverlode <65246540+CheetSheatOverlode@users.noreply.github.com>
2020-10-31 18:18:15 -0400

    Reinitialized tokens with ASCII

--- panda.py
+++ panda.py
@@ -1,33 +1,4 @@
-tokens = {
-    " ":"SPACE",
-    "\t":"TAB",
-    "\n":"EOL",
-    "\"":"DQUOTE",
-    "'":"SQUOTE",
-    ",":"COMMA",
-    ".":"DOT",
-    ":":"COLON",
-    ";":"SEMICOLON",
-    "+":"PLUS",
-    "-":"MINUS",
-    "*":"MUL",
-    "/":"DIV",
-    "^":"EXP",
-    "%":"MOD",
-    "#":"COM",
-    "(":"LPAREN",
-    ")":"RPAREN",
-    "[":"LBRACK",
-    "]":"RBRACK",
-    "{":"LBRACE",
-    "}":"RBRACE",
-    "@":"FUNC",
-    "$":"FORM",
-    "&":"AND",
-    "!":"NOT",
-    "|":"OR"
-}
-
+tokens = {i: chr(i) for i in range(128)}
 
 def lexer(string:str):
     global tokens
